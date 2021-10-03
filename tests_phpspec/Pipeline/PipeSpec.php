5170aed7673e3c103f1ede8fde6e23279a367d85
Josh Bruce <josh@8fold.pro>
2021-10-03 09:32:12 -0500

    Update PipeSpec.php

--- tests_phpspec/Pipeline/PipeSpec.php
+++ tests_phpspec/Pipeline/PipeSpec.php
@@ -5,10 +5,19 @@ namespace tests_phpspec\Eightfold\Foldable\Pipeline;
 use Eightfold\Foldable\Pipeline\Pipe;
 use PhpSpec\ObjectBehavior;
 
+use tests_phpspec\Eightfold\Foldable\Extensions\Append;
+
 class PipeSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType(Pipe::class);
+    }
+
+    public function it_can_be_folded_and_unfolded()
+    {
+        $this->beConstructedThrough("fold", ["Hello, World!"]);
+
+        $this->unfold()->shouldReturn("Hello, World!");
     }
 }
